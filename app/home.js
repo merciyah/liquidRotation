bc4c6ecb78cd2fed6d1766fc4c7df999b06aa654
Samuel <s.okoro07@gmail.com>
2020-10-23 15:23:40 +0100

    Button Animated. Rotation.

--- app/home.js
+++ app/home.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Animated } from 'react-native';
 
 const BUTTON_SIZE = 80;
 
-const Button = ({onPress}) => {
-	return(<TouchableOpacity style={{backgroundColor:"#333", justifyContent:"center", alignItems:"center", textAlign:'center', width:BUTTON_SIZE, height:BUTTON_SIZE, borderRadius:BUTTON_SIZE/2}}>
+const Button = ({onPress, animatedValue}) => {
+	return(<Animated.View style={{backgroundColor:"#333", justifyContent:"center", alignItems:"center", transform:[{rotateY: animatedValue.interpolate({inputRange:[0,0.5,1],outputRange:['0deg',"-90deg", "-180deg"]})}], textAlign:'center', width:BUTTON_SIZE, height:BUTTON_SIZE, borderRadius:BUTTON_SIZE/2}}>
+		<TouchableOpacity onPress = {onPress}>
 		<Text style={{color:"#ddd"}}>spin</Text>
-		</TouchableOpacity>)
+		</TouchableOpacity>
+		</Animated.View>)
 }
 
 const Home = (props) => {
+	const animatedValue = React.useRef(new Animated.Value(0)).current;
+	const onPress = () => {
+		Animated.timing(animatedValue, {
+			toValue: 1,
+			duration: 3000,
+			useNativeDriver: false
+		}).start()
+	}
   return (
     <View style = {{flex:1, justifyContent:'flex-end', padding:20, alignItems:'center'}}>
-    	<Button />
+    	<Button onPress = {onPress} animatedValue = {animatedValue} />
     </View>
   )
 }
