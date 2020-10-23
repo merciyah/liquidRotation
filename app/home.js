6d2b08a66f719a3f00cd3de1b0b6999f69fadca1
Samuel <s.okoro07@gmail.com>
2020-10-23 16:45:58 +0100

    Changing the background image

--- app/home.js
+++ app/home.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View, TouchableOpacity, Animated } from 'react-native
 const BUTTON_SIZE = 80;
 
 const Button = ({onPress, animatedValue}) => {
-	return(<Animated.View style={{backgroundColor:"#333", justifyContent:"center", alignItems:"center", transform:[
+	const inputRange = [0,0.001,0.5,0.501,1];
+	const circleBg = animatedValue.interpolate({
+		inputRange,
+		outputRange: ['#333', '#333', "#333",'#e62117','#e62117']
+	})
+	return(<Animated.View style={{backgroundColor:circleBg, justifyContent:"center", alignItems:"center", transform:[
 		{
 			perspective:400
 		},{
@@ -30,11 +35,17 @@ const Button = ({onPress, animatedValue}) => {
 }
 
 const Home = (props) => {
+
 	const animatedValue = React.useRef(new Animated.Value(0)).current;
 	const [index,setIndex] = React.useState(0);
+	const inputRange = [0,0.001,0.5,0.501,1];
+	const containerBg = animatedValue.interpolate({
+		inputRange,
+		outputRange: ['#e62117','#e62117','#e62117', '#333', "#333"]
+	})
 	const animation = (toValue) => Animated.timing(animatedValue, {
 			toValue,
-			duration: 3000,
+			duration: 2000,
 			useNativeDriver: false
 		})
 	const onPress = () => {
@@ -42,9 +53,9 @@ const Home = (props) => {
 		animation(index === 1 ? 0 : 1).start()
 	}
   return (
-    <View style = {{flex:1, justifyContent:'flex-end', padding:20, alignItems:'center'}}>
+    <Animated.View style = {{flex:1, backgroundColor:containerBg, justifyContent:'flex-end', padding:20, alignItems:'center'}}>
     	<Button onPress = {onPress} animatedValue = {animatedValue} />
-    </View>
+    </Animated.View>
   )
 }
 
